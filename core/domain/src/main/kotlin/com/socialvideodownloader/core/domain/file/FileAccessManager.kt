eace217a720c6f7b5ca057bd25ed7bdda1d08e79
arystan <arystantelbay@gmail.com>
2026-03-20 21:36:52 +0500

    style: fix pre-existing ktlint violations in core:domain

--- core/domain/src/main/kotlin/com/socialvideodownloader/core/domain/file/FileAccessManager.kt
+++ core/domain/src/main/kotlin/com/socialvideodownloader/core/domain/file/FileAccessManager.kt
@@ -4,6 +4,8 @@ import com.socialvideodownloader.core.domain.model.DownloadRecord
 
 interface FileAccessManager {
     suspend fun resolveContentUri(record: DownloadRecord): String?
+
     suspend fun isFileAccessible(contentUri: String): Boolean
+
     suspend fun deleteFile(contentUri: String): Boolean
 }
